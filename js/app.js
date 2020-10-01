858afc6544e35b3e4d7519e3bd6a2b0db2c5e6fe
David Mendoza <dmendoza99@unitec.ede>
2020-10-01 02:29:46 -0400

    hice unos cambios al js y agregue las tags necesarias para que sirva el toggle

--- js/app.js
+++ js/app.js
@@ -1,18 +1,18 @@
-// Selecciono el cuerpo del documento HTML (body) 
-var cuerpoweb = document.body; 
+// Selecciono el cuerpo del documento HTML (body)
+var cuerpoweb = document.body;
+let checkbox = document.getElementById("switchModo");
 
-// Obtengo el valor actual de la key 'modo' en localStorage 
-var modocolor = localStorage.getItem("modo"); 
+// Obtengo el valor actual de la key 'modo' en localStorage
+var modocolor = localStorage.getItem("modo");
 
-// Esta función carga el modo Oscuro o Claro, según el usuario haya configurado 
-function cargarModo() {    
-
-    if (modocolor === "oscuro") {               
-        cuerpoweb.classList.add("oscuro");
-    } else {
-        cuerpoweb.classList.add("claro");
-    }
-
+// Esta función carga el modo Oscuro o Claro, según el usuario haya configurado
+function cargarModo() {
+  checkbox.checked = modocolor === "oscuro";
+  if (modocolor === "oscuro") {
+    cuerpoweb.classList.add("oscuro");
+  } else {
+    cuerpoweb.classList.add("claro");
+  }
 }
 
 // Cuando el usuario presiona el botón, llama a la función que corresponde
@@ -20,32 +20,31 @@ function cargarModo() {
 var btnpresionado = false;
 
 function cambiarModo() {
+  if (btnpresionado) {
+    // Si No es presionado el botón
+    cuerpoweb.classList.remove("oscuro");
+    localStorage.setItem("modo", "claro");
+    checkbox.checked = false;
+    cuerpoweb.classList.add("claro");
+    btnpresionado = false;
+  } else {
+    // Si es presionado el botón
 
-    if (btnpresionado) { // Si No es presionado el botón 
-        cuerpoweb.classList.remove("oscuro");
-        localStorage.setItem("modo", "claro");
-        cuerpoweb.classList.add("claro");
-        btnpresionado = false;
-    } else { // Si es presionado el botón 
-
-        if (modocolor === "oscuro") {
-            resetear(); 
-            btnpresionado = true;           
-        } else {
-
-            cuerpoweb.classList.remove("claro");
-            localStorage.setItem("modo", "oscuro");
-            cuerpoweb.classList.add("oscuro");        
-            btnpresionado = true;
-
-        }        
+    if (modocolor === "oscuro") {
+      resetear();
+      btnpresionado = true;
+    } else {
+      cuerpoweb.classList.remove("claro");
+      checkbox.checked = true;
+      localStorage.setItem("modo", "oscuro");
+      cuerpoweb.classList.add("oscuro");
+      btnpresionado = true;
     }
+  }
 }
 
 // Reseteamos la configuración realizada y refrescamos la página (Esto es opcional, tu decides usarlo o no)
 function resetear() {
-
-    localStorage.removeItem('modo');
-    location.reload();
-
+  localStorage.removeItem("modo");
+  location.reload();
 }
